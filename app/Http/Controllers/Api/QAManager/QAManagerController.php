b8642257ea9f31195c3605e0264e4be6cf3bdb71
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-01-25 18:31:53 +0700

    [FEATURE][CATEGORIES] CREATE AND DELETE CATEGORIES]

--- app/Http/Controllers/Api/QAManager/QAManagerController.php
+++ app/Http/Controllers/Api/QAManager/QAManagerController.php
@@ -8,8 +8,41 @@ use Illuminate\Support\Facades\DB;
 
 class QAManagerController extends Controller
 {
-   public function createNewCategories()
-   {
-       DB::
-   }
+    /**
+     * Create new categories in resource
+     *
+     * @param \App\Http\Requests\Api\QA_Manager\StoreCategoryRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function createNewCategory(\App\Http\Requests\Api\QA_Manager\StoreCategoryRequest $request)
+    {
+        try {
+            DB::table('categories')->insert(
+                [
+                    'name' => $request->input('name'),
+                    'description' => $request->input('description')
+                ]
+            );
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 409);
+        }
+
+        // if success
+        return response()->json('Create new category success', '201');
+    }
+
+    /**
+     * Delete category from resource
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function deleteCategory($id)
+    {
+        try {
+            DB::table('categories')->delete($id);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 404);
+        }
+        return response()->json('Delete category success', '200');
+    }
 }
