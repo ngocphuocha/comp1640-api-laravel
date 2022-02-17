8fdd1a852801dce1d3bf7561ce45c78153d4b2aa
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-02-17 19:01:26 +0700

    refactror code

--- app/Http/Controllers/Api/Public/CategoryController.php
+++ app/Http/Controllers/Api/Public/CategoryController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Api\Public;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\QA_Manager\UpdateCategoryRequest;
 use App\Models\Category;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 
@@ -14,21 +15,21 @@ class CategoryController extends Controller
     {
         try {
             $categories = DB::table('categories')->select('id', 'name', 'description')->get();
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return response()->json($e->getMessage(), 404);
         }
 
         return response()->json($categories, 200);
     }
 
-    public function show($id)
+    public function show($id): JsonResponse
     {
         try {
             $category = Category::find($id);
             if (!$category) {
-                throw new \Exception("Category not found");
+                throw new Exception("Category not found");
             }
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return response()->json($e->getMessage(), Response::HTTP_NOT_FOUND);
         }
 
