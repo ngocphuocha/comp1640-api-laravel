c56b4c199f5a66a28f50b0b0eb344527a93bab63
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-02-18 09:41:53 +0700

    update feature download pdf

--- routes/api/public/public.php
+++ routes/api/public/public.php
@@ -13,7 +13,6 @@ Route::controller(\App\Http\Controllers\Api\Public\IdeaController::class)->group
     Route::get('/ideas/{id}/like/is-exist', 'checkIsExistLike')->middleware(['auth:sanctum']);
     Route::get('/ideas/{id}/download', 'downloadIdeaAsPDF'); // download pdf idea
     Route::get('/ideas/{idea}/likes', 'getTotalLikeOfIdea'); // get all like idea
-    Route::get('/ideas/{idea}/files', 'checkFilePDFIsExist');
     Route::post('/ideas/{idea}/likes', 'likeIdea')->middleware(['auth:sanctum']); // Like idea
     Route::delete('/ideas/{idea}/likes', 'unlikeIdea')->middleware(['auth:sanctum']); // delete like idea
 });
